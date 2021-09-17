9fb9c8f0640206771d13988ee00f4504992a416c
andrej00 <aprskalo00@gmail.com>
2021-09-17 08:32:48 +0200

    button color navigation

--- src/components/NavbarNavigation.vue
+++ src/components/NavbarNavigation.vue
@@ -2,14 +2,19 @@
     <div class="mt-4 pb-1">
         <b-row align-h="center">
             <b-col sm="3" class="mt-3">
-                <router-link to="/">
-                    <b-button variant="outline-primary">Opis Programa</b-button>
-                </router-link>
+                    <b-button to="/" :class="$route.name == 'OpisPrograma' ? 'button-active' : ''" variant="outline-primary">Opis Programa</b-button>
             </b-col>
 
             <b-col sm="3" class="mt-3">
-                <b-button to="/opiskolegija" variant="outline-primary">Opis Kolegija</b-button>
+                <b-button to="/opiskolegija" :class="$route.name == 'OpisKolegija' ? 'button-active' : ''" variant="outline-primary">Opis Kolegija</b-button>
             </b-col>
         </b-row>
     </div>
 </template>
+
+<style scoped>
+    .button-active {
+        color: #fff;
+        background-color: #0d6efd;
+    }
+</style>
